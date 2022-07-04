0d62c325c5f047227b45ae4444794719ad585c0d
LuisMars <LuisMars@users.noreply.github.com>
2022-07-04 18:15:38 +0000

    Deploying to gh-pages from @ LuisMars/blazelike@d1a689038adad547d026660195cd40027cd05f50 🚀

---
+++ js/game.js
@@ -0,0 +1,16 @@
+﻿const game = {
+    instance: false
+};
+window.init = (dotNetHelper) => {
+    console.log("Initializing");
+    game.instance = dotNetHelper;
+};
+window.onkeydown = (e) => {
+    if (!game.instance) {
+        console.log("game.instance is false");
+        return;
+    }
+    if (!e.repeat) {
+        game.instance.invokeMethodAsync('InvokeMove', e.keyCode);
+    }
+};
