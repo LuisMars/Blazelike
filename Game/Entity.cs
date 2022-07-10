79a1b217e489badd2ebacd1eaf339cd70f956326
luismars <luismars@hotmail.com>
2022-07-10 17:06:21 +0200

    Simple Map

--- Game/Entity.cs
+++ Game/Entity.cs
@@ -11,13 +11,20 @@ public class Entity
         Position = (x, y);
     }
 
-    public int BackgroundColor { get; init; } = 2;
+    public int BackgroundColor { get; private set; } = 2;
     public string Character { get; private set; }
-    public int Color { get; init; } = 4;
+    public int Color { get; private set; } = 4;
     public string Icon { get; private set; }
     public string Name { get; }
-    public (int X, int Y) Position { get; private set; }
-    public bool Walkable { get; }
+    public (int X, int Y) Position { get; internal set; }
+    public bool Walkable { get; private set; }
+
+    internal void SetAsDoor()
+    {
+        Walkable = true;
+        Icon = "door";
+        Character = "+";
+    }
 
     internal void SetPosition(int x, int y)
     {
--- Game/Extensions/TupleExtensions.cs
+++ Game/Extensions/TupleExtensions.cs
@@ -2,7 +2,21 @@
 
 public static class TupleExtensions
 {
+    public static bool Contains(this ((int X, int Y, int Z) Start, (int X, int Y, int Z) End) edge, (int X, int Y, int Z) point)
+    {
+        var vectorNormal = edge.Start.Vector(edge.End).Normalize();
+        var pointNormal = edge.Start.Vector(point).Normalize();
+        return vectorNormal.X == pointNormal.X &&
+               vectorNormal.Y == pointNormal.Y &&
+               vectorNormal.Z == pointNormal.Z;
+    }
+
     public static double Distance(this (int X, int Y, int Z) tuple, (int X, int Y, int Z) other)
+    {
+        return Math.Sqrt(tuple.DistanceSquared(other));
+    }
+
+    public static double Distance(this (double X, double Y, double Z) tuple, (double X, double Y, double Z) other)
     {
         return Math.Sqrt(tuple.DistanceSquared(other));
     }
@@ -13,5 +27,29 @@ public static class TupleExtensions
         var b = tuple.Y - other.Y;
         var c = tuple.Z - other.Z;
         return a * a + b * b + c * c;
+    }
+
+    public static double DistanceSquared(this (double X, double Y, double Z) tuple, (double X, double Y, double Z) other)
+    {
+        var a = tuple.X - other.X;
+        var b = tuple.Y - other.Y;
+        var c = tuple.Z - other.Z;
+        return a * a + b * b + c * c;
+    }
+
+    public static double Length(this (double X, double Y, double Z) tuple)
+    {
+        return Math.Sqrt(tuple.DistanceSquared((0, 0, 0)));
+    }
+
+    public static (double X, double Y, double Z) Normalize(this (double X, double Y, double Z) tuple)
+    {
+        var length = tuple.Length();
+        return (tuple.X / length, tuple.Y / length, tuple.Z / length);
+    }
+
+    public static (double X, double Y, double Z) Vector(this (int X, int Y, int Z) tuple, (int X, int Y, int Z) other)
+    {
+        return (other.X - tuple.X, other.Y - tuple.Y, other.Z - tuple.Z);
     }
 }
